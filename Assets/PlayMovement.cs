c2d86390d1816fbaaf93429bf2037048562b612a
manhdc6 <manhduongcong5@gmail.com>
2024-10-01 05:19:56 +0700

    Animtion and Movement

--- Assets/PlayMovement.cs
+++ Assets/PlayMovement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayMovement : MonoBehaviour
-{
+{   
+    public Animator animator;
     protected Rigidbody2D rb2d;
     public Vector2 velocity = new Vector2(0f,0f);
     public float pressHorizontal = 0f;
@@ -26,13 +27,24 @@ public class PlayMovement : MonoBehaviour
     }
     protected virtual void UpdateSpeed()
     {
-        this.velocity.x = this.pressHorizontal*this.moveSpeed;
-        if (this.IsFacingRight == false && this.pressHorizontal == 4)
+        this.velocity.x = this.pressHorizontal * this.moveSpeed;
+
+        // Kiểm tra xem có cần quay hướng nhân vật không
+        if (this.IsFacingRight && this.pressHorizontal < 0)
         {
-            transform.localScale = new Vector3(4, 3, 1);
+            // Quay hướng nhân vật sang trái
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            IsFacingRight = false;
         }
+        else if (!this.IsFacingRight && this.pressHorizontal > 0)
+        {
+            // Quay hướng nhân vật sang phải
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            IsFacingRight = true;
+        }
+
         this.velocity.y = this.pressVertical * this.moveSpeed;
-        this.rb2d.MovePosition(this.rb2d.position+this.velocity * Time.fixedDeltaTime);
-        
+        this.rb2d.MovePosition(this.rb2d.position + this.velocity * Time.fixedDeltaTime);
+        animator.SetFloat("Movement", Mathf.Abs(pressHorizontal));
     }
 }
